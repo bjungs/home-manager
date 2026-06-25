71a04d57f2d3943147f20609082701d8e6453656
bjungs <nunes.bjc@gmail.com>
2026-06-25 16:23:21 +0200

    removed unnecessary comment(s)

--- programs/zed.nix
+++ programs/zed.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     zed-editor
-    nil # language server for Nix
-    nixd # nix language server as well
+
+    # language server for Nix
+    nil
+    nixd
   ];
 }
