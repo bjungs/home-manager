d78aee65f5cf8bacf4dcf07ecb0e7b8816575325
bjungs <nunes.bjc@gmail.com>
2026-06-26 15:09:46 +0200

    docker bin redirect to podman

--- modules/podman.nix
+++ modules/podman.nix
@@ -4,12 +4,16 @@
     podman
     podman-compose
     podman-desktop
+
+    # creates an executable named 'docker' that calls podman instead
+    (writeShellScriptBin "docker" ''
+      exec ${podman}/bin/podman "$@"
+    '')
   ];
 
   home.shellAliases = {
     ll = "ls -lAh";
     py = "python";
     hm = "home-manager";
-    docker = "podman";
   };
 }
