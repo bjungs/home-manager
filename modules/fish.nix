a6931d9bc5b562ef4f3427b28e62b066014cab2a
bjungs <nunes.bjc@gmail.com>
2026-07-10 13:30:56 +0200

    small adjustment to keybindings in fish

--- modules/fish.nix
+++ modules/fish.nix
@@ -7,5 +7,15 @@
       starship init fish | source
       direnv hook fish | source
     '';
+
+    functions.fish_user_key_bindings = ''
+      # Shift+Home/End -> stop at punctuation
+      bind \e\[1\;5F forward-word
+      bind \e\[1\;5H backward-word
+
+      # Ctrl+Right/Left -> jump the whole arg
+      bind \e\[1\;5C forward-bigword
+      bind \e\[1\;5D backward-bigword
+    '';
   };
 }
