d28382e4a938490134bd614512f6b52359d51ebb
bjungs <nunes.bjc@gmail.com>
2026-09-17 09:28:00 +0200

    required tibia client dependencies

--- modules/tibia.nix
+++ modules/tibia.nix
@@ -1,11 +1,7 @@
 { pkgs, ... }:
 {
-  # currently Tibia package is not supported or maintained in nixpkgs, so we need to install its dependencies manually
+  # required dependencies for the Tibia client
   home.packages = with pkgs; [
-    libjpeg_turbo
-    libtiff
-
-    # require ms fonts
     corefonts
     vista-fonts
   ];
